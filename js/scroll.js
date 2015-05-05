a8b94bc5c586c419882de058ae191d8726e46c4c
Eric <empoole114@gmail.com>
2015-05-05 19:45:28 -0400

    Fixed scrolling for top navigation bar.

--- js/scroll.js
+++ js/scroll.js
@@ -1,14 +1,14 @@
 $('#about-link').click(function(event) {
 	event.preventDefault();
-	$('html, body').animate({scrollTop: $('#about').offset().top + 'px'}, 'slow');
+	$('html, body').animate({scrollTop: $('#about').offset().top - $('.header').outerHeight() + 'px'}, 'slow');
 });
 
 $('#projects-link').click(function(event) {
 	event.preventDefault();
-	$('html, body').animate({scrollTop: $('#projects').offset().top + 'px'}, 'slow');
+	$('html, body').animate({scrollTop: $('#projects').offset().top - $('.header').outerHeight() + 'px'}, 'slow');
 });
 
 $('#contact-link').click(function(event) {
 	event.preventDefault();
-	$('html, body').animate({scrollTop: $('#contact').offset().top + 'px'}, 'slow');
+	$('html, body').animate({scrollTop: $('#contact').offset().top - $('.header').outerHeight() + 'px'}, 'slow');
 });
